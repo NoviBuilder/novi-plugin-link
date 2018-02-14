d09709b894e439f7fdd8d4c28e7864ad0518ff7b
gap0n <gap0n@templatemonster.me>
2018-02-14 12:26:51 +0200

    FIX: excerpt function console removed

--- src/ExcerptFunction.jsx
+++ src/ExcerptFunction.jsx
@@ -1,5 +1,4 @@
 export function isLinkReplaceble(element){
-    console.log(element);
     if (!element || !element.hasAttribute("href") || !novi.element.hasAttribute(element, "href")) return false;
 
     return element.getAttribute("href") === novi.element.getAttribute(element, "href");
